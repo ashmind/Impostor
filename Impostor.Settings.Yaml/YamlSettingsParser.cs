49ad6f784beae8a3be2eaf6bf1d107f33a4e54c0
Andrey Shchekin <ashmind@gmail.com>
2015-04-18 14:23:18 +1200

    Removed unused code.

--- Impostor.Settings.Yaml/YamlSettingsParser.cs
+++ Impostor.Settings.Yaml/YamlSettingsParser.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using Impostor.Settings.Yaml.Internal;
 using InfoOf;
 using JetBrains.Annotations;
-using YamlDotNet.RepresentationModel;
 using YamlDotNet.Serialization;
 
 namespace Impostor.Settings.Yaml {
@@ -23,44 +22,6 @@ namespace Impostor.Settings.Yaml {
             if (settings == null) throw new ArgumentNullException("settings");
             var deserializer = new Deserializer(namingConvention: new MapNamingConvention(NameMap));
             return deserializer.Deserialize<ImpostorSettings>(new StringReader(settings));
-
-            //var yaml = new YamlStream();
-            //yaml.Load(new StringReader(settings));
-
-            //if (yaml.Documents.Count == 0)
-            //    throw Error("Found no YAML documents.");
-
-            //if (yaml.Documents.Count > 1)
-            //    throw Error("Found more than one YAML document.");
-
-            //var document = yaml.Documents.Single();
-            //var mapping = document.RootNode as YamlMappingNode;
-            //if (mapping == null)
-            //    throw Error("Expected root node to be a mapping (key: value).");
-
-            //var result = new ImpostorSettings();
-            //Apply(mapping, RootMap);
-
-            //return raw.ToSettings();
-        }
-
-        private static void Apply(YamlMappingNode mapping, IReadOnlyDictionary<string, Action<YamlNode>> appliers) {
-            foreach (var pair in mapping.Children) {
-                var keyScalar = pair.Key as YamlScalarNode;
-                if (keyScalar == null)
-                    throw Error("Expected key to be a scalar, found '{0}' ({1}).", pair.Key, pair.Key.GetType());
-
-                Action<YamlNode> apply;
-                if (!appliers.TryGetValue(keyScalar.Value, out apply))
-                    throw Error("Unexpected key '{0}'.", keyScalar.Value);
-
-                apply(pair.Value);
-            }
-        }
-
-        [StringFormatMethod("message")]
-        private static FormatException Error(string message, params object[] args) {
-            return new FormatException(string.Format(message, args));
         }
     }
 }
